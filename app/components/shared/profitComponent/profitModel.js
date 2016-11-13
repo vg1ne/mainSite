2cd20bbc5c7776874e45699c51d324a8dcda87b9
vgoreiko <mbox696@gmail.com>
2016-11-13 22:18:53 +0200

    removed *.js and *.js.map files generated by IDE

--- app/components/shared/profitComponent/profitModel.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var Profit = (function () {
-    function Profit() {
-    }
-    return Profit;
-}());
-exports.Profit = Profit;
-//# sourceMappingURL=profitModel.js.map
--- app/components/shared/serviceComponent/serviceItemModel.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var ServiceItem = (function () {
-    function ServiceItem() {
-    }
-    return ServiceItem;
-}());
-exports.ServiceItem = ServiceItem;
-//# sourceMappingURL=serviceItemModel.js.map

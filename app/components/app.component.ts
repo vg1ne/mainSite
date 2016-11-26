d7146dbf82919b575197a881135eead3c9098951
vg1ne <mbox696@gmail.com>
2016-11-26 16:51:14 +0200

    changed styles of main page

--- app/components/app.component.ts
+++ app/components/app.component.ts
@@ -2,7 +2,7 @@ import {Component} from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template:`<main-header></main-header><section class="container"><router-outlet></router-outlet></section><main-footer></main-footer>`
+    template:`<main-header></main-header><section class="top-header"><router-outlet></router-outlet></section><main-footer></main-footer>`
 })
 
 export class AppComponent {}

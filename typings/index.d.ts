58726fcba31100a222a8c4c2bcd39d617ff8d293
vgoreiko <vgoreiko@deloitte.com>
2016-11-28 09:32:04 +0200

    fixed typings

---
+++ typings/index.d.ts
@@ -0,0 +1,6 @@
+/// <reference path="globals/es6-shim/index.d.ts" />
+declare var require: {
+    <T>(path: string): T;
+    (paths: string[], callback: (...modules: any[]) => void): void;
+    ensure: (paths: string[], callback: (require: <T>(path: string) => T) => void) => void;
+};

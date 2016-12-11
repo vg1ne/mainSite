9ed0caab6a0ca22d0993ccc37a321bd94f2bddde
vg1ne <mbox696@gmail.com>
2016-12-11 16:29:35 +0200

    added scroll top on navigation end

--- app/components/app.component.ts
+++ app/components/app.component.ts
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core';
 import {TranslateService} from 'ng2-translate';
-import { MainService } from "./../services/main.service"
+import { MainService } from "./../services/main.service";
+import { Router, NavigationEnd } from '@angular/router';
 require("./../../styles/style.less")
 require("./../../styles/mediaQueries.less")
 
@@ -10,10 +11,21 @@ require("./../../styles/mediaQueries.less")
 })
 
 export class AppComponent {
-    constructor(private translate: TranslateService, private mainService: MainService) {
+    constructor(private translate: TranslateService,
+                private mainService: MainService,
+                private router: Router) {
         translate.addLangs(mainService.possibleLanguages);
         translate.setDefaultLang(mainService.currentChosenLanguage);
         translate.use(mainService.currentChosenLanguage);
+    }
+
+    ngOnInit() {
+        this.router.events.subscribe((evt) => {
+            if (!(evt instanceof NavigationEnd)) {
+                return;
+            }
+            document.body.scrollTop = 0;
+        });
     }
 }
 

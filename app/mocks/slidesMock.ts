510ea9a3bf3d3655361abd2add240a290cdd6222
vg1ne <mbox696@gmail.com>
2017-02-11 18:07:19 +0200

    changed images for slides

--- app/mocks/slidesMock.ts
+++ app/mocks/slidesMock.ts
@@ -2,7 +2,7 @@
     home: [{
         id: 1,
         active: true,
-        imgUrl: '../../../styles/images/header1.jpg',
+        imgUrl: '../../../styles/images/slide-1.jpg',
         title: 'Brand',
         heading: 'A Fresh view',
         text: 'at the common knowledge',
@@ -11,47 +11,37 @@
     {
         id: 2,
         active: false,
-        imgUrl: '../../../styles/images/img_chania2.jpg',
+        imgUrl: '../../../styles/images/slide-2.jpg',
         title: 'UI/UX design',
-        heading: 'A Fresh view',
-        text: 'at the common knowledge',
-        desc: 'A brand is a candy paper wrap - the exterior of your business. It is the First thing your potential cusomer visualy sees.The significal of branding can`t be overemphasized: we won`t buy a car which we don`t line and we don`t care how many hourses there under the hood.'
+        heading: 'Functional',
+        text: 'decisions for you business.',
+        desc: 'We explore your market, occupation and analyze your requests. We always look for the most effective solutions in terms of investments and of course never forget about the possibility of innovations which can make your webpage maximum effective and profitable.'
     },
     {
         id: 3,
         active: false,
-        imgUrl: '../../../styles/images/img_chania.jpg',
+        imgUrl: '../../../styles/images/slide-3.jpg',
         title: 'Web development',
-        heading: 'A Fresh view',
-        text: 'at the common knowledge',
-        desc: 'A brand is a candy paper wrap - the exterior of your business. It is the First thing your potential cusomer visualy sees.The significal of branding can`t be overemphasized: we won`t buy a car which we don`t line and we don`t care how many hourses there under the hood.'
+        heading: 'From many small gears ',
+        text: 'into a single driving force',
+        desc: 'We develop innovative high-tech projects that have successfully carried out the tasks. From promotional sites to heavy-information portals. Sites that inform, to advise that sell. Which meets all the trends of time.'
     },
     {
         id: 4,
         active: false,
-        imgUrl: '../../../styles/images/img_chania2.jpg',
+        imgUrl: '../../../styles/images/slide-4.jpg',
         title: 'Mobile development',
-        heading: 'A Fresh view',
-        text: 'at the common knowledge',
-        desc: 'A brand is a candy paper wrap - the exterior of your business. It is the First thing your potential cusomer visualy sees.The significal of branding can`t be overemphasized: we won`t buy a car which we don`t line and we don`t care how many hourses there under the hood.'
+        heading: 'Promote.',
+        text: 'Share. Affect.',
+        desc: 'Global web statistics indicate that over 1 in 5 web visitors are now using a mobile device when browsing the web. We can help you add a fun and engaging mobile experience to your existing website today or we can help you redesign your website to be mobile friendly from the ground up.'
     },
     {
         id: 5,
         active: false,
-        imgUrl: '../../../styles/images/img_chania.jpg',
+        imgUrl: '../../../styles/images/slide-5.jpg',
         title: 'Content Creation',
-        heading: 'A Fresh view',
-        text: 'at the common knowledge',
-        desc: 'A brand is a candy paper wrap - the exterior of your business. It is the First thing your potential cusomer visualy sees.The significal of branding can`t be overemphasized: we won`t buy a car which we don`t line and we don`t care how many hourses there under the hood.'
-    },
-    // {
-    //     id: 6,
-    //     active: false,
-    //     imgUrl: '../../../styles/images/img_flower.jpg',
-    //     title: 'Workflow',
-    //     heading: 'A Fresh view',
-    //     text: 'at the common knowledge',
-    //     desc: 'A brand is a candy paper wrap - the exterior of your business. It is the First thing your potential cusomer visualy sees.The significal of branding can`t be overemphasized: we won`t buy a car which we don`t line and we don`t care how many hourses there under the hood.'
-    // }
-    ]
+        heading: 'Silence is not gold ',
+        text: 'the network does not forgive silence.',
+        desc: 'If you are not online, you basically do not exist. What first appears in your mind when you hear a combination of the words "business and the Internet"? Correct, the website.'
+    }]
 }

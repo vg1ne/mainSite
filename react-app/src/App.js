add7f2573898a56fdc1e39b872f257a6ab59ef57
vgoreiko <vgoreiko@deloitte.com>
2017-07-03 16:55:44 +0300

    react starter

---
+++ react-app/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+    <section className="app">
+      <div className="preloader-wrapper">
+        <div className="preloader-fill"></div>
+        <div className="preloader-img"></div>
+      </div>
+      <section>Hello</section>
+    </section>
+    );
+  }
+}
+
+export default App;

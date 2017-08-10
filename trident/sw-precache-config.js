54a21e24211c296210d6502fd67a88c6a910ebc9
vgoreiko <vgoreiko@deloitte.com>
2017-08-10 15:02:11 +0300

    config

---
+++ trident/sw-precache-config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  navigateFallback: '/index.html',
+  stripPrefix: 'dist',
+  root: 'dist/',
+  staticFileGlobs: [
+    'dist/index.html',
+    'dist/**.js',
+    'dist/**.css',
+    'dist/i18n/*.json',
+    'dist/assets/styles/images/*.*',
+    'dist/**.**'
+  ]
+};
